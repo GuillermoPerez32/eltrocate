830dd59754c8ee7ff84efd95dc0f4772650e8d66
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2024-07-29 11:52:54 -0400

    feat: Add Post and Comment models and migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(10)->create();
+
+        $posts = Post::factory(50)->create();
+        $posts->each(function ($post) {
+            Comment::factory(rand(0, 3))->create([
+                'post_id' => $post->id,
+                'user_id' => User::inRandomOrder()->first()->id,
+            ]);
+        });
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'is_admin' => true,
         ]);
     }
 }
